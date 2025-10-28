7957cba89bb17331d771a25b52239f7518205070
gu1gor <gustavoigorof@gmail.com>
2025-10-27 23:08:40 -0300

    chore: adicionar estrutura inicial do projeto Spring Boot

---
+++ src/main/java/com/gustavoigor/cadastro_jogadores_uol/CadastroJogadoresUolApplication.java
@@ -0,0 +1,13 @@
+package com.gustavoigor.cadastro_jogadores_uol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroJogadoresUolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroJogadoresUolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavoigor/cadastro_jogadores_uol/CadastroJogadoresUolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoigor.cadastro_jogadores_uol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroJogadoresUolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
