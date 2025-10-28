113277fcd0e025ce246e1c8a48d8fea25cc548d5
gu1gor <gustavoigorof@gmail.com>
2025-10-28 15:14:51 -0300

    feat: criar entidade Jogador

---
+++ src/main/java/com/gustavoigor/cadastro_jogadores_uol/model/Jogador.java
@@ -0,0 +1,28 @@
+package com.gustavoigor.cadastro_jogadores_uol.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+public class Jogador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    private String nome;
+    @NonNull
+    private String email;
+    private String telefone;
+
+    /* Atributos controlados pelo sistema
+    preenchidos quando o cadastro for feito */
+    private String codinome;
+    private List<String> listaReferencia;    //
+}
